6a94b29cb017e85a8cb844b588731104d4b48815
mirzadham <215553@student.upm.edu.mym>
2026-03-16 12:00:05 +0800

    fix: remove invalid Bootstrap CDN integrity hashes blocking CSS/JS loading

--- views/layout/admin_header.php
+++ views/layout/admin_header.php
@@ -8,7 +8,6 @@
     <!-- Bootstrap 5 CSS (CDN) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
           rel="stylesheet" 
-          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YcnS/1WR6zNkzlAOIuSR0638MYECfKJfcGr" 
           crossorigin="anonymous">
 
     <!-- Bootstrap Icons -->
